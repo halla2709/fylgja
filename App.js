f7a512e3338b8dfd9b9f7fdc13c8d6b2a9b7079c
Halla <halla2709@gmail.com>
2018-02-20 20:54:37 +0000

    added png logo image

--- App.js
+++ App.js
@@ -1,23 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TouchableOpacity } from 'react-native';
+import { Font } from 'expo'; 
 
 export default class App extends React.Component {
-  render() {
+  
+  state = {
+    fontLoaded: false
+  };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'merriweather-black': require('./source/styles/fonts/Merriweather-Black.ttf'),
+      'dosis-medium': require('./source/styles/fonts/Dosis-Medium.ttf'),
+      'opensans-regular': require('./source/styles/fonts/OpenSans-Regular.ttf'),
+    });
+    this.setState({ fontLoaded: true });
+  }
+
+  render(){
     return (
+      
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      {
+        this.state.fontLoaded ? (
+        <Text style={styles.title}> Fylgja </Text>
+        ) : null
+      }
+
+{
+        this.state.fontLoaded ? (
+        <Text style={styles.h1}> Ég er fyrirsögn </Text>
+        ) : null
+      }
+
+      {
+        this.state.fontLoaded ? (
+      <Text style={styles.body}> Góðann dag, ég er fylgjan</Text>
+    ) : null
+  }
+      <TouchableOpacity style={styles.buttons} onPress={() => {Alert.alert("Þú vinnur!")}}> 
+      <Text>Veldu mig!</Text> 
+      </TouchableOpacity>
+
       </View>
     );
   }
 }
+  
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(239,239,239)',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around'
   },
+
+  title: {
+    fontFamily: 'merriweather-black',
+    fontSize: 50
+  },
+  
+  buttons: {
+    width: 300,
+    backgroundColor: '#DDDDDD',
+    alignItems: 'center',
+    padding: 10,
+    borderColor: '#000',
+    borderWidth: 1
+
+  },
+
+  h1: {
+    fontFamily: 'dosis-medium',
+    fontSize: 25
+  },
+
+  body: {
+    fontFamily: 'opensans-regular',
+    fontSize: 18
+  }
 });
