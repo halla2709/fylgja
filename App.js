f0ece6f4c8760adc27d46e6e1e62b25251c73ba5
Halla <halla2709@gmail.com>
2018-03-04 17:11:19 +0000

    áfram með navigation

--- App.js
+++ App.js
@@ -1,38 +1,5 @@
 import React from 'react';
-import { StackNavigator, DrawerNavigator } from 'react-navigation';
-import { StyleSheet, Text, View, Button, Alert, TouchableOpacity, TouchableHighlight } from 'react-native';
-import { Font } from 'expo'; 
-import { HomeScreen } from './src/screens/HomeScreen.js';
-import { SearchScreen } from './src/screens/SearchScreen.js';
-
-const NavStack = StackNavigator({
-  Home: {
-    screen: HomeScreen,
-  },
-  Search: {
-    screen: SearchScreen
-  }
-},
-{
-  initialRouteName: 'Home',
-  headerMode: 'float',
-  navigationOptions: {
-    headerStyle: {
-      backgroundColor: 'rgb(34,82,171)'
-    },
-    headerTintColor: '#fff',
-    title: 'Welcome!'
-  }
-})
-
-const RootStack = DrawerNavigator({
-  Nav: { screen: NavStack }
-},
-{
-  navigationOptions: {
-    drawerLabel: 'Drawer'
-  }
-});
+import RootStack from './src/controllers/ApplicationNavigation.js'
 
 export default class App extends React.Component {
   render() {
