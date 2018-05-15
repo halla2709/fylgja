e3676bfd2c068f5dbd074418e601993d67145ec8
Halla <halla2709@gmail.com>
2018-05-15 22:20:14 +0000

    searchready!!

---
+++ src/controllers/SearchHelper.js
@@ -0,0 +1,43 @@
+import Chapters from "../assets/testContent/chapters.js";
+
+function SearchChapterTitles(key) {
+    let chaptersToReturn = [];
+    Chapters.forEach(chapter => {
+        let subChaptersThatMatchSearch = findMatchingSubChapters(chapter, key);
+        if (subChaptersThatMatchSearch.length > 0) {
+            let matchingChapter = {
+                key: chapter.key,
+                name: chapter.name,
+                subchapters: subChaptersThatMatchSearch
+            }
+            chaptersToReturn.push(matchingChapter);
+        }
+        else if (chapterTitleMatchesSearch(chapter, key)) {
+            let matchingChapter = {
+                key: chapter.key,
+                name: chapter.name
+            }
+            chaptersToReturn.push(matchingChapter);
+        }
+    });
+
+    return chaptersToReturn;
+}
+
+function findMatchingSubChapters(chapter, key) {
+    let subChaptersToReturn = [];
+    chapter.subchapters.forEach(subchapter => {
+        if (chapterTitleMatchesSearch(subchapter, key)) {
+            subChaptersToReturn.push({ key: subchapter.key, name: subchapter.name });
+        }
+    });
+    return subChaptersToReturn;
+}
+
+function chapterTitleMatchesSearch(chapter, key) {
+    return chapter.name.toLowerCase().contains(key.toLowerCase());
+}
+
+export {
+    SearchChapterTitles
+};
